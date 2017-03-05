bf3fee81089211da88f69f8bd70774122e271a83
Danielle Carrick <daniellecarrick@Danielles-MacBook-Air.local>
2017-03-05 15:19:50 +0200

    can add. delete and complete goals

--- public/js/controllers/goalsController.js
+++ public/js/controllers/goalsController.js
@@ -1,4 +1,25 @@
 app.controller('goalsController', function($scope, goalsFactory){
   $scope.goals = goalsFactory.goals;
-  console.log("we work");
+  $scope.completed = goalsFactory.completed;
+  /*variables in the form*/
+  $scope.goalText;;
+  $scope.typeText;
+  $scope.descriptionText;
+  $scope.addGoal = function() {
+    $scope.goals.push({goal:$scope.goalText, type: $scope.typeText, description: $scope.descriptionText});
+  }
+  $scope.moveToCompleted = function(goal) {
+    $scope.completed.push(goal);
+    var index = $scope.goals.indexOf(goal);
+    $scope.goals.splice(index, 1);
+    console.log("it was clicked");
+  }
+  $scope.delete = function(goal) {
+    /*To do: add an are you sure*/
+    var index = $scope.goals.indexOf(goal);
+    $scope.goals.splice(index, 1);
+  }
+  $scope.toggleGoalsView = function() {
+    $scope.selected = true;
+  }
 })
