be8fd5bc9766e420ed4703f60a84c3ffda99f250
Danielle Carrick <daniellecarrick@Danielles-MacBook-Air.local>
2017-03-08 17:12:27 +0200

    working on Add a Goal overlay. Still buggy

--- public/js/services/goalsFactory.js
+++ public/js/services/goalsFactory.js
@@ -17,6 +17,24 @@ app.factory('goalsFactory', function(){
     type: "project",
     description: "Plants are dope",
     image: "http://lorempixel.com/400/300/cats/"
+  },
+  {
+    goal: "Live abroad",
+    type: "travel",
+    description: "For ze experience",
+    image: "http://lorempixel.com/400/300/cats/"
+  },
+  {
+    goal: "Go skydiving",
+    type: "experience",
+    description: "No time for fear",
+    image: "http://lorempixel.com/400/300/cats/"
+  },
+  {
+    goal: "Adopt a dog",
+    type: "project",
+    description: "Dogs are hella cute",
+    image: "http://lorempixel.com/400/300/cats/"
   }
   ];
 
